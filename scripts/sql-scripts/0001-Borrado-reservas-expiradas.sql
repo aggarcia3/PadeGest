19ec34ea1551efdd3eb305e432e59174af913810
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-11-05 14:52:00 +0100

    Evento borrado reservas expiradas: corregir indentación

--- scripts/sql-scripts/0001-Borrado-reservas-expiradas.sql
+++ scripts/sql-scripts/0001-Borrado-reservas-expiradas.sql
@@ -5,13 +5,13 @@ DROP EVENT IF EXISTS `PADEGEST`.`limpieza_reservas_expiradas`;
 
 DELIMITER $$
 CREATE EVENT `PADEGEST`.`limpieza_reservas_expiradas`
-	ON SCHEDULE EVERY 1 MINUTE STARTS CURRENT_TIMESTAMP
+    ON SCHEDULE EVERY 1 MINUTE STARTS CURRENT_TIMESTAMP
     ON COMPLETION NOT PRESERVE
     DO BEGIN
-		DECLARE duracion TIME;
-		SELECT `duracionReservas` FROM `PADEGEST`.`configuracion` LIMIT 1 INTO duracion;
-		DELETE LOW_PRIORITY
-			FROM `PADEGEST`.`reserva`
-			WHERE ADDTIME(`fecha`, duracion) <= NOW();
-	END$$
+        DECLARE duracion TIME;
+        SELECT `duracionReservas` FROM `PADEGEST`.`configuracion` LIMIT 1 INTO duracion;
+        DELETE LOW_PRIORITY
+            FROM `PADEGEST`.`reserva`
+            WHERE ADDTIME(`fecha`, duracion) <= NOW();
+    END$$
 DELIMITER ;
